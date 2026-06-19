be89d773fc81310cc9dd231a594fc6b838cbfca3
amisadman <inboxsadman@gmail.com>
2026-06-19 12:05:00 +0600

    feat: create button and navigation UI components

---
+++ resources/js/Components/PrimaryButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+    type: {
+        type: String,
+        default: 'submit',
+    },
+});
+</script>
+
+<template>
+    <button
+        :type="type"
+        class="inline-flex items-center px-4 py-2 bg-[#1f2b5b] border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-[#3dbdec] hover:text-[#1f2b5b] focus:bg-[#1f2b5b] active:bg-[#1f2b5b] focus:outline-none focus:ring-2 focus:ring-[#3dbdec] focus:ring-offset-2 transition ease-in-out duration-150"
+    >
+        <slot />
+    </button>
+</template>
