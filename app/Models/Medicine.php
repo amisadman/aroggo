bd3c6679bb5e543bf622e75c1cedea755cfe4d11
amisadman <inboxsadman@gmail.com>
2026-06-21 16:10:00 +0600

    feat: create medicines inventory schema with quantity tracker

---
+++ app/Models/Medicine.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Medicine extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'company_id',
+        'pack_details',
+        'quantity',
+        'price',
+        'category_id',
+        'location_id',
+        'status',
+    ];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+}
